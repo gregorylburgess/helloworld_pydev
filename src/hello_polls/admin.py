382c3014549839b71c7e2637f8e64e8f5c58b88a
gregorylburgess <gregory.l.burgess@gmail.com>
2013-01-08 20:43:04 -1000

    Initial Commit.

---
+++ src/hello_polls/admin.py
@@ -0,0 +1,18 @@
+from hello_polls.models import MyPoll, MyChoice
+from django.contrib import admin
+
+class MyChoiceInline(admin.TabularInline):
+    model = MyChoice
+    extra = 3
+
+class MyPollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [MyChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+admin.site.register(MyPoll, MyPollAdmin)
